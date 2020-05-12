1d77c79c25d364850b488040acf8c2e48456f270
Jesse Wallace <jesse.rudolf.wallace@gmail.com>
2020-05-12 15:06:35 +1000

    Add generatewindelchanges.py

---
+++ generatewindelchanges.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import csv
+import sys
+
+header = sys.stdin.readline().strip().split()
+reader = csv.DictReader(sys.stdin, fieldnames=header, dialect="excel-tab")
+
+ref_name = None
+diff = 0
+for line in reader:
+    if line["ref_name"] != ref_name:
+        ref_name = line["ref_name"]
+        diff = 0
+        print("new contig", file=sys.stderr)
+
+    original_coord = f"{ref_name}:{line['position']}"
+    if line["deletion"] == "True" and int(line["indel_length"]) > 1:
+        original_coord += f"-{int(line['position']) + int(line['indel_length']) - 1}"
+
+    new_coord = f"{ref_name}:{int(line['position']) + diff}"
+    if line["insertion"] == "True" and int(line["indel_length"]) > 1:
+        new_coord += f"-{int(line['position']) + diff + int(line['indel_length']) - 1}"
+
+    print(f"{original_coord} {new_coord}")
+
+    print(diff, line["w_length_mode"], file=sys.stderr)
+    diff += int(line["w_length_mode"])
